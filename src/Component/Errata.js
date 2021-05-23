bcb8430047bd83b45a3e790022a382dc9b72606f
황정민 <2016026126@hanyang.ac.kr>
2021-05-23 11:46:59 +0900

    add errata design

--- src/Component/Errata.js
+++ src/Component/Errata.js
@@ -1,12 +1,38 @@
 import React from "react";
 
 const Errata = ({ num, answer, select, result }) => {
+  const style = {
+    correct: {
+      backgroundColor: "#FF5F08",
+      color: "white",
+      borderRadius: "6px",
+      margin: "0px 4px",
+    },
+    wrong: {
+      backgroundColor: "#0A0863",
+      color: "white",
+      borderRadius: "6px",
+      margin: "0px 4px",
+    },
+  };
+
+  const checkAnswer = (result) => {
+    if (result == "O") {
+      return style.correct;
+    } else if (result == "X") {
+      return style.wrong;
+    } else {
+      return;
+    }
+  };
   return (
     <div className="errata">
       <div className="errata-num">{num}</div>
-      <div className="errata-answer">{answer}</div>
-      <div className="errata-select">{select}</div>
-      <div className="errata-result">{result}</div>
+      <div style={checkAnswer(result)}>
+        <div className="errata-answer">{answer}</div>
+        <div className="errata-select">{select}</div>
+        <div className="errata-result">{result}</div>
+      </div>
     </div>
   );
 };
